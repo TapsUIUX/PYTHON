8195b96e15d62f38c2bd5f919074c8a1550c2caf
TapsUIUX <25495356+TapsUIUX@users.noreply.github.com>
2019-01-20 17:48:02 -0500

    Comparison

---
+++ comparison.py
@@ -0,0 +1,28 @@
+# Comparison Operator 
+# eqaiuality ==
+
+
+x = (2==2) #true
+
+x = ('2'==2) #false
+
+x = (2!=2) #false
+
+x = (2>=2) #True
+
+x = (2>2) #Flase
+
+
+# Logical Operators 
+# and , or, not
+
+
+x = ((1<2) and (1<1)) # False
+x = ((1<2) or (1<1)) # True
+x = ((1<2) and not(1<1)) # True
+
+
+print("----------------------")
+print(x)
+print("----------------------")
+
